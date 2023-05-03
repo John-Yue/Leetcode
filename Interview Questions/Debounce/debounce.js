4ca35155dffce2e010afda1f603909070cdce0b7
岳晨飞 <yuechenfei@boswinner.com>
2023-05-03 10:34:12 +0800

    add debounce func

--- Interview Questions/Debounce/debounce.js
+++ Interview Questions/Debounce/debounce.js
@@ -3,3 +3,26 @@
  * 创建一个防抖函数，该函数从上次被调用后，延迟wait毫秒后调用func方法。
  *
  */
+
+function sample_debounce(fn, wait) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(fn, wait);
+  };
+}
+
+console.log(sample_debounce);
+// 简单防抖函数测试
+// function test() {
+//   console.log("debounced func");
+// }
+//
+// const debouncedFunc = sample_debounce(test, 500);
+//
+// const intervalTimerId = setInterval(debouncedFunc, 499);
+//
+// setTimeout(() => {
+//   clearInterval(intervalTimerId);
+//   clearTimeout(intervalTimerId);
+// }, 5 * 1000);
