077780a534bd8df33d7ebc4a951f8ddd37c73208
岳晨飞 <yuechenfei@boswinner.com>
2023-05-09 10:07:50 +0800

    优化flat函数，添加深度，使用reduce解决

--- Interview Questions/handWrite/flatten.js
+++ Interview Questions/handWrite/flatten.js
@@ -1,14 +1,27 @@
-function flatten(arr) {
+function flatten(arr, d = 0) {
   let result = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      result = result.concat(flatten(arr[i]));
-    } else {
-      result = result.concat(arr[i]);
-    }
-  }
+  (function flat(arr, d) {
+    arr.forEach((item) => {
+      if (Array.isArray(item) && d > 0) {
+        flat(item, d - 1);
+      } else {
+        result.push(item);
+      }
+    });
+  })(arr, d);
   return result;
 }
 
+// 添加深度的flat 数组展平
+function flat(arr, d = 0) {
+  return d > 0
+    ? arr.reduce(
+        (prev, curr) =>
+          prev.concat(Array.isArray(curr) ? flat(curr, d - 1) : curr),
+        []
+      )
+    : arr.slice();
+}
+
 const arr = [1, [2, [3, 4]]];
-console.log(flatten(arr));
+console.log(flatten(arr, 1));
